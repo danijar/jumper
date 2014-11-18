8938ac88daaa174497f2ac311db7fbcdbf29dea0
Danijar Hafner <mail@danijar.org>
2014-11-18 11:46:11 +0100

    Add entity manager
    Add sprite, body and movement properties
    Create a bunch of balls
    Increase window size

--- src/main.py
+++ src/main.py
@@ -1,19 +1,47 @@
-import sys, pygame, time
+import sys, pygame, time, uuid, random
 
 # Initialize application
 pygame.init()
-width, height = 320, 240
+width, height = 800, 600
 pygame.display.set_icon(pygame.image.load("asset/other/icon.png"))
 pygame.display.set_caption("Window")
 pygame.display.set_mode((width, height))
 screen = pygame.display.get_surface()
 
-# Create ball
-sprite = pygame.image.load("asset/texture/circle.png")
-body = sprite.get_rect()
-direction = [1, 1]
-speed = 2
+# Movement property
+class Movement(object):
+	def __init__(self, direction=[0, 0], speed=1):
+		self.direction = direction
+		self.speed = speed
 
+# Property collections
+class Entities(object):
+	def __init__(self):
+		self.sprites = {}
+		self.bodies = {}
+		self.movements = {}
+	def create(self):
+		return uuid.uuid4()
+	def remove(self, entity):
+		self.sprites.pop(entity, None)
+		self.bodies.pop(entity, None)
+		self.movements.pop(entity, None)
+
+entities = Entities()
+
+# Create ball
+def create_ball():
+	entity = entities.create()
+	entities.sprites[entity] = pygame.image.load("asset/texture/circle.png")
+	entities.bodies[entity] = entities.sprites[entity].get_rect()
+	entities.movements[entity] = Movement([1, 1])
+	return entity
+
+for i in range(10):
+	entity = create_ball()
+	entities.movements[entity].speed = (i + 1)
+
+# Main loop
 running = True
 start = 0
 while running:
@@ -33,16 +61,30 @@ while running:
 			elif event.key == pygame.K_c and event.mod & pygame.KMOD_LCTRL:
 				running = False
 
-	# Move ball and let it bounce from window borders
-	body = body.move([x * speed for x in direction])
-	if body.left < 0 or body.right > width:
-		direction[0] = -direction[0]
-	if body.top < 0 or body.bottom > height:
-		direction[1] = -direction[1]
+	# Update move properties
+	for entity in entities.movements:
+		movement = entities.movements.get(entity)
+		body = entities.bodies.get(entity)
+		if body:
+			# Move in current direction with current speed
+			way = [x * movement.speed for x in movement.direction]
+			body = body.move(way)
+			# Bounce from window borders
+			if body.left < 0 or body.right > width:
+				movement.direction[0] *= -1
+			if body.top < 0 or body.bottom > height:
+				movement.direction[1] *= -1
+			# Store result
+			entities.movements[entity] = movement
+			entities.bodies[entity] = body
 
-	# Render sprite
+	# Render sprites
 	screen.fill((0, 0, 0))
-	screen.blit(sprite, body)
+	for entity in entities.sprites:
+		sprite = entities.sprites.get(entity)
+		body = entities.bodies.get(entity)
+		if body:
+			screen.blit(sprite, body)
 	pygame.display.flip()
 
 	# Sleep until frame ends
